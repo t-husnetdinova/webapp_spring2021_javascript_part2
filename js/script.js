bc35eff55eefba59e9bbf1583e2ca2fee24573e4
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-02-24 21:35:58 -0700

    finalized visibilty toggle on dropdown event

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
+// function to toggle the visibility of the travel question
 function updateCountryView() {
+    // grab the elements by id
     var travel = document.getElementById("travel");
     var divCountry = document.getElementById("divCountry");
 
+    // if yes is selected
     if(travel.value == "Yes") {
         divCountry.classList.remove("invisible");
+    }
+    // if they change their mind
+    else {
+        divCountry.classList.add("invisible");
     }
 }
